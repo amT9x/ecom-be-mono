6127a9ddd18419cfb83096beb3f6d028bec36a5f
amT9x <hoangvantung155@gmail.com>
2026-04-19 15:29:54 +0700

    feat(logger): auto inject request context using pino mixin

--- src/infrastructure/logger/logger.ts
+++ src/infrastructure/logger/logger.ts
@@ -1,5 +1,12 @@
 import { pino } from "pino";
 import { loggerConfig } from "../../config/logger.config.js";
+import { requestContext } from "../context/request-context.js";
 
 // Instance of pino
-export const logger = pino(loggerConfig);
+export const logger = pino({
+  ...loggerConfig,
+
+  mixin() {
+    return requestContext.get() ?? {};
+  },
+});
