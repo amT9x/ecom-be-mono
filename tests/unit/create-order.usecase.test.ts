410f8d8e7b9f89c8e4aac9ef0abd8986e3668af9
amT9x <hoangvantung155@gmail.com>
2026-05-14 22:55:35 +0700

    chore(testing): add mock for create order

--- tests/unit/create-order.usecase.test.ts
+++ tests/unit/create-order.usecase.test.ts
@@ -1,7 +1,53 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { Pool } from 'pg';
+import { InventoryService } from '../../src/modules/inventory/inventory.service.js';
+import { InventoryRepository } from '../../src/modules/inventory/inventory.repository.js';
+import { CreateOrderUseCase } from '../../src/modules/orders/create-order.usecase.js';
 
 describe('CreateOrderUseCase', () => {
-  it('should create order successfully', () => {
+  let client: any;
+  let pool: any;
+
+  let inventoryService: any;
+  let orderRepo: any;
+
+  let inventoryFactory: any;
+  let orderRepoFactory: any;
+
+  let orderUsecase: CreateOrderUseCase;
+
+  // ⭐ Test setup
+  beforeEach(() => {
+    client = {
+      query: vi.fn(),
+      release: vi.fn(),
+    };
+
+    pool = {
+      connect: vi.fn().mockResolvedValue(client),
+    };
+
+    inventoryService = {
+      reserveStock: vi.fn(),
+    };
+
+    orderRepo = {
+      create: vi.fn(),
+      addItem: vi.fn(),
+    };
+
+    inventoryFactory = vi.fn().mockReturnValue(inventoryService);
+
+    orderRepoFactory = vi.fn().mockReturnValue(orderRepo);
+
+    // -----------------------
+    // 5️⃣ Create usecase
+    //
+    // -----------------------
+    orderUsecase = new CreateOrderUseCase(inventoryFactory, orderRepoFactory, pool);
+  });
+
+  it('should create order successfully', async () => {
     expect(true).toBeTruthy();
   });
   it('should rollback if reserve stock fails', () => {
