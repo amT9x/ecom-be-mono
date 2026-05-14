e78426639cd74fb751eeeff32d56c0f6d5996690
amT9x <hoangvantung155@gmail.com>
2026-05-14 20:48:17 +0700

    feat(testing): skeleton create-order.usecase

---
+++ tests/unit/create-order.usecase.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect, vi } from 'vitest';
+
+describe('CreateOrderUseCase', () => {
+  it('should create order successfully', () => {
+    expect(true).toBeTruthy();
+  });
+  it('should rollback if reserve stock fails', () => {
+    expect(true).toBeTruthy();
+  });
+  it('should rollback if create order fails', () => {
+    expect(true).toBeTruthy();
+  })
+  it('should release client', () => {
+    expect(true).toBeTruthy();
+  });
+});
