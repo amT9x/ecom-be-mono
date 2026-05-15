ae9085f5367863a8f0c3164a386c31a774de0b8b
amT9x <hoangvantung155@gmail.com>
2026-05-15 13:33:28 +0700

    chore(testing): introduce confirm order test

---
+++ tests/intergration/confirm-order.usecase.test-int.ts
@@ -0,0 +1,13 @@
+import { describe, beforeEach, it, expect } from "vitest"
+
+describe('ConfirmOrderUseCase test-int', () => {
+  beforeEach(() => {})
+
+  it('should confirm pending order', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should reject confirm when order already confirmed', () => {
+    expect(true).toBe(true);
+  });
+})
