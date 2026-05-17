f15833927ebc07057900a22d36104a8ee968bc32
amT9x <hoangvantung155@gmail.com>
2026-05-17 22:03:59 +0700

    feat(cancel-order): add full cancel-order
    - usecase and testing

--- tests/intergration/cancel-order.usecase.int.test.ts
+++ tests/intergration/cancel-order.usecase.int.test.ts
@@ -1,7 +1,65 @@
-import { describe, it, expect } from 'vitest';
+import { describe, beforeEach, it, expect } from 'vitest';
+
+import { testPool, resetDatabase } from '../setup/test_db';
+
+import { CancelOrderUseCase } from '../../src/modules/orders/cancel-order.usecase';
+
+import { ORDER_ID, ORDER_STATUS, TOTAL_AMOUNT_ORDER } from '../setup/constanst';
+
+async function seedOrder(status = ORDER_STATUS.PENDING) {
+  await testPool.query(
+    `
+    INSERT INTO orders(id, status, total_amount)
+    VALUES ($1, $2, $3)
+  `,
+    [ORDER_ID, status, TOTAL_AMOUNT_ORDER],
+  );
+}
 
 describe('CancelOrderUseCase test-int', () => {
-  it('should cancel order', () => {
+  beforeEach(async () => {
+    await resetDatabase();
+  });
+
+  it('should cancel pending order', async () => {
+    await seedOrder(ORDER_STATUS.PENDING);
+
+    const usecase = new CancelOrderUseCase(testPool);
+
+    const result = await usecase.execute(ORDER_ID);
+
+    expect(result.status).toBe(ORDER_STATUS.CANCELLED);
+
+    const res = await testPool.query(`SELECT status FROM orders WHERE id=$1`, [
+      ORDER_ID,
+    ]);
+
+    expect(res.rows[0].status).toBe(ORDER_STATUS.CANCELLED);
+  });
+
+  it('should cancel confirmed order', async () => {
+    await seedOrder(ORDER_STATUS.CONFIRMED);
+
+    const usecase = new CancelOrderUseCase(testPool);
+
+    const result = await usecase.execute(ORDER_ID);
+
+    expect(result.status).toBe(ORDER_STATUS.CANCELLED);
+  });
+
+  //future
+  it('should reject cancel when shipped', async () => {
     expect(true).toBe(true);
-  })
-})
+    // await seedOrder('SHIPPED');
+
+    // const usecase = new CancelOrderUseCase(testPool);
+
+    // await expect(usecase.execute(ORDER_ID)).rejects.toThrow();
+  });
+
+  it('should throw when order not found', async () => {
+    const usecase = new CancelOrderUseCase(testPool);
+
+    await expect(usecase.execute('not-exist')).rejects.toThrow();
+  });
+});
