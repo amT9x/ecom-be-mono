d987d06ed6f39abbe742f9fa50a82f9c6d16bcf4
amT9x <hoangvantung155@gmail.com>
2026-05-17 20:17:50 +0700

    feat(testing): add full confirm-order test

--- tests/intergration/confirm-order.usecase.int.test.ts
+++ tests/intergration/confirm-order.usecase.int.test.ts
@@ -1,10 +1,38 @@
-import { describe, beforeEach, it, expect } from "vitest"
+import { describe, beforeEach, it, expect, beforeAll } from "vitest"
+import { testPool, resetDatabase } from "../setup/test_db"
+import { ConfirmOrderUseCase } from "../../src/modules/orders/confirm-order.usecase"
+
+const orderId = '11111111-1111-1111-1111-111111111111';
+const status = 'PENDING';
+
+async function seedOrder() {
+  await testPool.query(
+    `
+    INSERT INTO orders(id, status)
+    VALUES ($1, $2)
+  `,
+    [orderId, status],
+  );
+}
 
 describe('ConfirmOrderUseCase test-int', () => {
-  beforeEach(() => {})
+  beforeEach(async () => {
+    await resetDatabase();
+    await seedOrder();
+  })
 
-  it('should confirm pending order', () => {
-    expect(true).toBe(true);
+  it('should confirm pending order', async () => {
+    const usecase = new ConfirmOrderUseCase(testPool);
+
+    const result = await usecase.execute('order-1');
+
+    expect(result.status).toBe('CONFIRMED');
+
+    const res = await testPool.query(
+      `SELECT status FROM orders WHERE id='order-1'`,
+    );
+
+    expect(res.rows[0].status).toBe('CONFIRMED');
   });
 
   it('should reject confirm when order already confirmed', () => {
