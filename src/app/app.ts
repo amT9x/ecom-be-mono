f2083611f344d689d47f3e11f1757d550cf6e1c5
amT9x <hoangvantung155@gmail.com>
2026-05-21 22:08:56 +0700

    chore(server): update api /

--- src/app/app.ts
+++ src/app/app.ts
@@ -20,7 +20,7 @@ export function buildApp() {
   app.register(productRoutes);
 
   app.get('/', async () => {
-    return { app: 'ecom_tunghv' };
+    return { app: 'ecom' };
   });
 
   app.get('/health', async (req) => {
