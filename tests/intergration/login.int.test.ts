0167bd38e2b72ae767792b9a7a276f116f84a013
amT9x <hoangvantung155@gmail.com>
2026-06-01 08:34:36 +0700

    fix(test-int): miss param refreshtokenrepository in loginUsecase

--- tests/intergration/login.int.test.ts
+++ tests/intergration/login.int.test.ts
@@ -2,18 +2,26 @@ import { describe, it, expect, beforeEach } from "vitest";
 import { resetDatabase, resetDatabaseUser, testPool } from "../setup/test_db";
 import { PasswordService } from "../../src/shared/security/password.service";
 import { JwtService } from "../../src/shared/security/jwt.service";
+import { PostgresRefreshTokenRepository } from "../../src/modules/auth/infrastructure/postgres-refresh-token.repository";
 import { PostgresUserRepository } from "../../src/modules/user/infrastructure/postgres-user.repository";
 import { LoginUseCase } from "../../src/modules/auth/application/login.usecase";
 import { USER } from "../setup/constanst";
 import { seedUser } from "../setup/seed-test";
 
 describe("Login", () => {
-  const password_hash = new PasswordService();
-  const jwttoken = new JwtService();
-  const repositoryUser = new PostgresUserRepository(testPool);
-  const loginUsecase = new LoginUseCase(repositoryUser, password_hash, jwttoken);
+  let loginUsecase: LoginUseCase;
   beforeEach( async () => {
     await resetDatabaseUser();
+    const password_hash = new PasswordService();
+    const jwttoken = new JwtService();
+    const repositoryUser = new PostgresUserRepository(testPool);
+    const repositoryRefreshToken = new PostgresRefreshTokenRepository(testPool);
+    loginUsecase = new LoginUseCase(
+      repositoryUser,
+      password_hash,
+      jwttoken,
+      repositoryRefreshToken,
+    );
     await seedUser(testPool, USER.id, USER.email, USER.password, USER.full_name);
   });
 
